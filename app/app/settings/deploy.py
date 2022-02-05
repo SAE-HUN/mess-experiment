e7c18c4f5331af75dbd247192806f9cead35a6fb
SAE-HUN <devhun58@gmail.com>
2022-02-06 01:54:17 +0900

    test

---
+++ app/app/settings/deploy.py
@@ -0,0 +1,6 @@
+from .common import *
+
+ALLOWED_HOSTS = ['round-table.ap-northeast-2.elasticbeanstalk.com']
+
+
+WSGI_APPLICATION = 'app.wsgi.deploy.application'
---
+++ app/app/settings/develop.py
@@ -0,0 +1,6 @@
+from .common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+WSGI_APPLICATION = 'app.wsgi.develop.application'
